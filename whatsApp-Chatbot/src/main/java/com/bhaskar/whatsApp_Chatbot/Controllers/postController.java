052a609a509901ecc72bd54c3282818f6021bc85
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-31 15:33:39 +0530

    ContollerChecked

--- whatsApp-Chatbot/src/main/java/com/bhaskar/whatsApp_Chatbot/Controllers/postController.java
+++ whatsApp-Chatbot/src/main/java/com/bhaskar/whatsApp_Chatbot/Controllers/postController.java
@@ -1,6 +1,19 @@
 package com.bhaskar.whatsApp_Chatbot.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/")
 public class postController {
+
+
+
+    //check
+//    @GetMapping
+//    public String greet(){
+//        return "heyhey";
+//    }
 
 }
