e33fe7aad30ddbd87f64b5681f9f5807b41b999b
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-31 16:09:55 +0530

    service-layer

--- whatsApp-Chatbot/src/main/java/com/bhaskar/whatsApp_Chatbot/Service/service.java
+++ whatsApp-Chatbot/src/main/java/com/bhaskar/whatsApp_Chatbot/Service/service.java
@@ -1,8 +1,17 @@
 package com.bhaskar.whatsApp_Chatbot.Service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class service {
+
+    private static Logger logger = LoggerFactory.getLogger(service.class);
+
+
+
+
 }
