ea93233c00935b6f76bc2ca7f17f2da3eeec18cc
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-31 15:40:35 +0530

    REQ-DTO

--- whatsApp-Chatbot/src/main/java/com/bhaskar/whatsApp_Chatbot/DTOs/MessageRequest.java
+++ whatsApp-Chatbot/src/main/java/com/bhaskar/whatsApp_Chatbot/DTOs/MessageRequest.java
@@ -1,4 +1,13 @@
 package com.bhaskar.whatsApp_Chatbot.DTOs;
 
+import lombok.Data;
+
+@Data
 public class MessageRequest {
+    \
+    @NotBlank(message = "message must be at least of single character")
+    private String message;
+
+    //getters and setters will be invoked by @Data itself .. added mvn dependency
+
 }
