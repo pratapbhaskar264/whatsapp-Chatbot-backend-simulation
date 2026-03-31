90221af203d9b093a5c634a1e99cbe237404090d
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-31 15:28:46 +0530

    first-Commit

---
+++ whatsApp-Chatbot/src/main/java/com/bhaskar/whatsApp_Chatbot/WhatsAppChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.bhaskar.whatsApp_Chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsAppChatbotApplication.class, args);
+	}
+
+}
---
+++ whatsApp-Chatbot/src/test/java/com/bhaskar/whatsApp_Chatbot/WhatsAppChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhaskar.whatsApp_Chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
