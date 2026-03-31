d0bdd1bfd5beb65ce489a00769c0544f4fa08560
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-31 16:03:37 +0530

    overall-working

--- whatsApp-Chatbot/src/main/java/com/bhaskar/whatsApp_Chatbot/Controllers/postController.java
+++ whatsApp-Chatbot/src/main/java/com/bhaskar/whatsApp_Chatbot/Controllers/postController.java
@@ -2,6 +2,7 @@ package com.bhaskar.whatsApp_Chatbot.Controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class postController {
 
     private static final Logger logger = LoggerFactory.getLogger(postController.class);
+
+    private final Service service;
+
+    //constructor injection for tight coupling
+    public postController(Service service) {
+        this.service = service;
+    }
 
 
     //check
--- whatsApp-Chatbot/src/main/java/com/bhaskar/whatsApp_Chatbot/Service/service.java
+++ whatsApp-Chatbot/src/main/java/com/bhaskar/whatsApp_Chatbot/Service/service.java
@@ -1,5 +1,8 @@
 package com.bhaskar.whatsApp_Chatbot.Service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+@Service
 public class service {
 }
