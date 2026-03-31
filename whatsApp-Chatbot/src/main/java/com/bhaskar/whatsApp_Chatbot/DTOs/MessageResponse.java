952b392078ea06a904195d5b933f8d89bf5bdc55
pratabhaskar264 <pratapbhaskar0264@gmail.com>
2026-03-31 15:44:08 +0530

    RES-DTO-completed

--- whatsApp-Chatbot/src/main/java/com/bhaskar/whatsApp_Chatbot/DTOs/MessageResponse.java
+++ whatsApp-Chatbot/src/main/java/com/bhaskar/whatsApp_Chatbot/DTOs/MessageResponse.java
@@ -1,4 +1,12 @@
 package com.bhaskar.whatsApp_Chatbot.DTOs;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class MessageResponse {
+    private String reply;
 }
